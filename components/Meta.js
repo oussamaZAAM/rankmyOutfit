11b55a0cd1bc4ffe6e0b63639b4ac8ed14d6173f
oussamaZAAM <zaam.oussama@gmail.com>
2023-01-22 01:13:44 +0100

    Actual first commit

---
+++ components/Meta.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import React from "react";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
+      <meta charSet="UTF-8" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+    title: 'WebDev',
+    keywords: 'web developement, programming',
+    description: 'We keep track of every nook and cranny of WebDev',
+}
+
+export default Meta;
---
+++ components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {MdOutlineImageSearch} from 'react-icons/md'
+
+const Nav = () => {
+  return (
+    <div className='grid grid-cols-8 w-full h-16'>
+        <div class="grid grid-cols-8 justify-center items-center h-full col-start-0 col-span-8 lg:col-start-2 lg:col-span-6">
+            <div className='flex justify-center items-center | col-span-5 sm:col-span-4 md:col-span-2'>
+                <MdOutlineImageSearch color='#DB76DC' size={30} />
+                <h1 className='text-xl font-black font-display' style={{color: '#DB76DC'}}>rankmyOutfit</h1>
+            </div>
+            <div className='hidden md:flex col-span-2'></div>
+            <div className='flex justify-evenly items-center | invisible sm:visible  | hidden sm:flex col-span-2'>
+                <h5 className='text-sm font-bold font-display' style={{color: '#DB76DC'}}>How it works</h5>
+                <h5 className='text-sm font-bold font-display' style={{color: '#DB76DC'}}>Contact</h5>
+            </div>
+            <div className='flex justify-center items-center col-span-3 sm:col-span-2'>
+                <div className='rounded-full w-24 sm:w-32 h-10 flex justify-center items-center' style={{backgroundColor: '#6F1AB6'}}>
+                    <h5 className='font-display font-bold text-white text-sm'>Sign in</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
