8f3bdc2b3854e5e35714fb51bfe280e868f4f7c5
oussamaZAAM <zaam.oussama@gmail.com>
2023-01-31 17:37:12 +0100

    New Outfit - Upload Images

--- pages/new-outfit.js
+++ pages/new-outfit.js
@@ -1,10 +1,57 @@
 import Head from "next/head";
+import { useState } from "react";
 
-import empty from "../public/images/empty.jpg";
+import { MdAdd } from "react-icons/md";
+import { RiImageEditLine } from "react-icons/ri";
+import { TiCancel } from "react-icons/ti";
 
 import styles from "../styles/Home.module.css";
 
 const newOutfit = () => {
+  const [images, setImages] = useState(["", "", "", ""]);
+
+  //Functions
+  const handleChange = async (e, index) => {
+    const file = e.target.files[0];
+    console.log(index);
+    const base64 = await convertToBase64(file);
+    setImages((prevImages) => {
+      const newImages = [...prevImages];
+      newImages[index] = base64;
+      return newImages;
+    });
+    e.target.value = "";
+  };
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      if (!file) {
+        alert("Please pick an image");
+      } else {
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          resolve(fileReader.result);
+        };
+      }
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const deleteImage = (e, index) => {
+    e.preventDefault();
+    setImages((prevImages) => {
+      // const newImages = prevImages.filter(
+      //   (image) => prevImages.indexOf(image) !== index
+      // );
+      var newImages = [...prevImages];
+      newImages[index] = '';
+      return newImages;
+    });
+  };
+
   return (
     <>
       <Head>
@@ -31,60 +78,221 @@ const newOutfit = () => {
               </h3>
 
               <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4">
+                {/* Image 1  */}
                 <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
-                  {/* <img
-                    className="block object-cover w-full h-full rounded-3xl"
-                    src={empty.src}
-                    alt=""
-                  /> */}
-                  <div className="block object-cover w-full h-full rounded-3xl bg-gray-500"></div>
-                  <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
-                    <div className="flex justify-center items-center ">
-                      <p className="font-title font-bold text-white text-base">Empty</p>
+                  {images[0] ? (
+                    <img
+                      className="block object-cover w-full h-full rounded-3xl bg-gray-500"
+                      src={images[0]}
+                    />
+                  ) : (
+                    <div className="block object-cover w-full h-full rounded-3xl bg-gray-500">
+                      <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-100 group-hover:scale-x-[0.25] transition duration-100">
+                        {/* <div className="flex justify-center items-center h-full w-full"> */}
+                        <p className="font-title font-bold text-white text-base text-center">
+                          Empty
+                        </p>
+                        {/* </div> */}
+                      </div>
                     </div>
-                  </div>
-                </div>
-                
-                <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
-                  {/* <img
-                    className="block object-cover w-full h-full rounded-3xl"
-                    src={empty.src}
-                    alt=""
-                  /> */}
-                  <div className="block object-cover w-full h-full rounded-3xl bg-gray-500"></div>
+                  )}
                   <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
-                    <div className="flex justify-center items-center ">
-                      <p className="font-title font-bold text-white text-base">Empty</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
-                  {/* <img
-                    className="block object-cover w-full h-full rounded-3xl"
-                    src={empty.src}
-                    alt=""
-                  /> */}
-                  <div className="block object-cover w-full h-full rounded-3xl bg-gray-500"></div>
-                  <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
-                    <div className="flex justify-center items-center ">
-                      <p className="font-title font-bold text-white text-base">Empty</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
-                  {/* <img
-                    className="block object-cover w-full h-full rounded-3xl"
-                    src={empty.src}
-                    alt=""
-                  /> */}
-                  <div className="block object-cover w-full h-full rounded-3xl bg-gray-500"></div>
-                  <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
-                    <div className="flex justify-center items-center ">
-                      <p className="font-title font-bold text-white text-base">Empty</p>
+                    <div className="flex justify-center items-center h-full w-full">
+                      {!images[0] ? (
+                        <label htmlFor="upload0">
+                          <MdAdd size={30} color="white" />
+                          <input
+                            onChange={(e) => handleChange(e, 0)}
+                            type="file"
+                            id="upload0"
+                            accept="image/*"
+                            style={{ display: "none" }}
+                          />
+                        </label>
+                      ) : (
+                        <div className="flex justify-center items-center space-x-8">
+                          <label htmlFor="reupload0">
+                            <RiImageEditLine size={25} color="white" />
+                            <input
+                              onChange={(e) => handleChange(e, 0)}
+                              type="file"
+                              id="reupload0"
+                              accept="image/*"
+                              style={{ display: "none" }}
+                            />
+                          </label>
+                          <TiCancel
+                            onClick={(e) => deleteImage(e, 0)}
+                            size={25}
+                            color="white"
+                          />
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
 
+                {/* Image 2 */}
+                <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
+                  {images[1] ? (
+                    <img
+                      className="block object-cover w-full h-full rounded-3xl bg-gray-500"
+                      src={images[1]}
+                    />
+                  ) : (
+                    <div className="block object-cover w-full h-full rounded-3xl bg-gray-500">
+                      <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-100 group-hover:scale-x-[0.25] transition duration-100">
+                        {/* <div className="flex justify-center items-center h-full w-full"> */}
+                        <p className="font-title font-bold text-white text-base text-center">
+                          Empty
+                        </p>
+                        {/* </div> */}
+                      </div>
+                    </div>
+                  )}
+                  <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
+                    <div className="flex justify-center items-center h-full w-full">
+                      {!images[1] ? (
+                        <label htmlFor="upload1">
+                          <MdAdd size={30} color="white" />
+                          <input
+                            onChange={(e) => handleChange(e, 1)}
+                            type="file"
+                            id="upload1"
+                            accept="image/*"
+                            style={{ display: "none" }}
+                          />
+                        </label>
+                      ) : (
+                        <div className="flex justify-center items-center space-x-8">
+                          <label htmlFor="reupload1">
+                            <RiImageEditLine size={25} color="white" />
+                            <input
+                              onChange={(e) => handleChange(e, 1)}
+                              type="file"
+                              id="reupload1"
+                              accept="image/*"
+                              style={{ display: "none" }}
+                            />
+                          </label>
+                          <TiCancel
+                            onClick={(e) => deleteImage(e, 1)}
+                            size={25}
+                            color="white"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+                {/* Image 3 */}
+                <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
+                  {images[2] ? (
+                    <img
+                      className="block object-cover w-full h-full rounded-3xl bg-gray-500"
+                      src={images[2]}
+                    />
+                  ) : (
+                    <div className="block object-cover w-full h-full rounded-3xl bg-gray-500">
+                      <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-100 group-hover:scale-x-[0.25] transition duration-100">
+                        {/* <div className="flex justify-center items-center h-full w-full"> */}
+                        <p className="font-title font-bold text-white text-base text-center">
+                          Empty
+                        </p>
+                        {/* </div> */}
+                      </div>
+                    </div>
+                  )}
+                  <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
+                    <div className="flex justify-center items-center h-full w-full">
+                      {!images[2] ? (
+                        <label htmlFor="upload2">
+                          <MdAdd size={30} color="white" />
+                          <input
+                            onChange={(e) => handleChange(e, 2)}
+                            type="file"
+                            id="upload2"
+                            accept="image/*"
+                            style={{ display: "none" }}
+                          />
+                        </label>
+                      ) : (
+                        <div className="flex justify-center items-center space-x-8">
+                          <label htmlFor="reupload2">
+                            <RiImageEditLine size={25} color="white" />
+                            <input
+                              onChange={(e) => handleChange(e, 2)}
+                              type="file"
+                              id="reupload2"
+                              accept="image/*"
+                              style={{ display: "none" }}
+                            />
+                          </label>
+                          <TiCancel
+                            onClick={(e) => deleteImage(e, 2)}
+                            size={25}
+                            color="white"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+                {/* Image 4 */}
+                <div className="relative rounded-3xl mobile:w-72 mobile:h-96 cursor-pointer transition duration-300 group hover:opacity-75 transition duration-200">
+                  {images[3] ? (
+                    <img
+                      className="block object-cover w-full h-full rounded-3xl bg-gray-500"
+                      src={images[3]}
+                    />
+                  ) : (
+                    <div className="block object-cover w-full h-full rounded-3xl bg-gray-500">
+                      <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-100 group-hover:scale-x-[0.25] transition duration-100">
+                        {/* <div className="flex justify-center items-center h-full w-full"> */}
+                        <p className="font-title font-bold text-white text-base text-center">
+                          Empty
+                        </p>
+                        {/* </div> */}
+                      </div>
+                    </div>
+                  )}
+                  <div className="absolute inset-y-2/4 w-full h-6 bg-black scale-x-0 group-hover:scale-x-100 transition duration-200">
+                    <div className="flex justify-center items-center h-full w-full">
+                      {!images[3] ? (
+                        <label htmlFor="upload3">
+                          <MdAdd size={30} color="white" />
+                          <input
+                            onChange={(e) => handleChange(e, 3)}
+                            type="file"
+                            id="upload3"
+                            accept="image/*"
+                            style={{ display: "none" }}
+                          />
+                        </label>
+                      ) : (
+                        <div className="flex justify-center items-center space-x-8">
+                          <label htmlFor="reupload3">
+                            <RiImageEditLine size={25} color="white" />
+                            <input
+                              onChange={(e) => handleChange(e, 3)}
+                              type="file"
+                              id="reupload3"
+                              accept="image/*"
+                              style={{ display: "none" }}
+                            />
+                          </label>
+                          <TiCancel
+                            onClick={(e) => deleteImage(e, 3)}
+                            size={25}
+                            color="white"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <div className="flex justify-center items-center my-4">
