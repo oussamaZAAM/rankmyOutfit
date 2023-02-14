bb50748196faee02bef93b442bd234d641bfd1d9
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-14 23:03:22 +0100

    Add login/logout functionnalities to navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,15 @@
 import Link from 'next/link'
-import React from 'react'
+
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { useState } from 'react';
+import Image from 'next/image';
+
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleDropdown = () => setIsOpen(!isOpen)
+  
+  const {data: session, status} = useSession();
   return (
     <div className='grid grid-cols-8 w-full h-16'>
         <div className="grid grid-cols-8 justify-center items-center h-full col-start-0 col-span-8 lg:col-start-2 lg:col-span-6">
@@ -15,9 +23,39 @@ const Nav = () => {
                 <h5 className='text-sm font-bold font-display' style={{color: '#DB76DC'}}>Contact</h5>
             </div>
             <div className='flex justify-center items-center col-span-3 sm:col-span-2'>
-                <div className='rounded-full w-24 sm:w-32 h-10 flex justify-center items-center' style={{backgroundColor: '#6F1AB6'}}>
-                    <Link href='/signin'><h5 className='font-display font-bold text-white text-sm'>Sign in</h5></Link>
-                </div>
+                {status !== 'authenticated'
+                    ? <div className='rounded-full w-24 sm:w-32 h-10 flex justify-center items-center' style={{backgroundColor: '#6F1AB6'}}>
+                        <Link href='/signin'><h5 className='font-display font-bold text-white text-sm'>Sign in</h5></Link>
+                    </div>
+                    : <div className='rounded-full w-24 sm:w-32 h-10 flex'>
+                        <div className="relative z-50">
+                            <button onClick={toggleDropdown} className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" type="button">
+                                <span className="sr-only">Open user menu</span>
+                                <Image height={50} width={50} className="w-8 h-8 rounded-full" src={session.user.image} alt="user photo" />
+                            </button>
+                            <div className={"z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 "+(isOpen ? '' : 'hidden')}>
+                                <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                                <div>{session.user.name}</div>
+                                <div className="font-medium truncate">{session.user.email}</div>
+                                </div>
+                                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
+                                <li>
+                                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                                </li>
+                                </ul>
+                                <div className="py-2">
+                                <button onClick={()=>signOut()} className="w-full text-left block px-4 py-2 text-sm text-gray-700 hover:text-red-500 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     </div>
@@ -25,3 +63,13 @@ const Nav = () => {
 }
 
 export default Nav
+
+export const getServerSideProps = async (context) => {
+    const session = await getSession(context);
+
+    return {
+        props: {
+            session
+        }
+    }
+}
