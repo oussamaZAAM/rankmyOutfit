bcbdeb2ca947ad8c07558f54f3aea695faf0cb5e
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-14 23:30:48 +0100

    Add Facebook Login

--- pages/signin.js
+++ pages/signin.js
@@ -124,10 +124,10 @@ const Signin = () => {
               </div>
               <div className="flex items-center justify-between">
                   <div className={"flex items-center justify-center mobile:w-20 mobile:h-20 rounded-2xl drop-shadow-xl border border-my-pink1 cursor-pointer group "+styles.boxshadow}>
-                      <FcGoogle onClick={()=>signIn()} className="w-12 mobile:w-14 h-12 mobile:h-14 group-hover:animate-wiggle" />
+                      <FcGoogle onClick={()=>signIn("google")} className="w-12 mobile:w-14 h-12 mobile:h-14 group-hover:animate-wiggle" />
                   </div>
                   <div className={"flex items-center justify-center mobile:w-20 mobile:h-20 rounded-2xl drop-shadow-xl border border-my-pink1 cursor-pointer group "+styles.boxshadow}>
-                      <FaFacebook color='#1877F2' className="w-12 mobile:w-14 h-12 mobile:h-14 group-hover:animate-wiggle" />
+                      <FaFacebook onClick={()=>signIn("facebook")} color='#1877F2' className="w-12 mobile:w-14 h-12 mobile:h-14 group-hover:animate-wiggle" />
                   </div>
                   <div className={"flex items-center justify-center mobile:w-20 mobile:h-20 rounded-2xl drop-shadow-xl border border-my-pink1 cursor-pointer group "+styles.boxshadow}>
                       <FaTwitter color='#55ACEE' className="w-12 mobile:w-14 h-12 mobile:h-14 group-hover:animate-wiggle" />
