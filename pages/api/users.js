12a7b973989ef409133a2a207c9e3179a557c0b5
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-17 13:14:47 +0100

    Restrict authorization to token bearer

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,18 +1,27 @@
 import Users from "@/model/Schema";
+import { verify } from "jsonwebtoken";
 
 export default async function Handler(req, res) {
-    if (req.method === 'PUT') {
-        const user = await Users.findOne({email: req.body.email});
+    try {
+        const { cookies } = req;
+        const jwt = cookies.authentication;
+        verify(jwt, process.env.JWT_SECRET);
 
-        if (!user) return res.status(404).json({message: 'User not found'});
+        if (req.method === 'PUT') {
+            const user = await Users.findOne({email: req.body.email});
 
-        const edit = await Users.updateOne(
-            {email: req.body.email},
-            { $set: {"image.url": req.body.image.url, "image.delete": req.body.image.url, "image.position": req.body.image.position}}
-        )
+            if (!user) return res.status(404).json({message: 'User not found'});
 
-        if (!(edit && edit.matchedCount)) return res.status(503).json({message: 'Database Error'});
+            const edit = await Users.updateOne(
+                {email: req.body.email},
+                { $set: {"image.url": req.body.image.url, "image.delete": req.body.image.url, "image.position": req.body.image.position}}
+            )
 
-        return res.status(200).json({message: 'Image updated successfully'});
+            if (!(edit && edit.matchedCount)) return res.status(503).json({message: 'Database Error'});
+
+            return res.status(200).json({message: 'Image updated successfully'});
+        }
+    } catch (e) {
+        return res.status(501).json({message: "Invalid Token"});
     }
 }
