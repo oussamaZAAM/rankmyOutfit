28bb54b6e676df579af55815f724700b54a1533d
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-17 02:32:52 +0100

    Update Object inside Mongodb Document

--- pages/api/users.js
+++ pages/api/users.js
@@ -2,14 +2,14 @@ import Users from "@/model/Schema";
 
 export default async function Handler(req, res) {
     if (req.method === 'PUT') {
-        console.log(req.body)
         const user = await Users.findOne({email: req.body.email});
 
-        if (!user) return res.status(503).json({message: 'Database Error'});
+        if (!user) return res.status(404).json({message: 'User not found'});
+        console.log(req.body)
 
         const edit = await Users.updateOne(
             {email: req.body.email},
-            { $set: {image: req.body.image}}
+            { $set: {"image.url": req.body.image.url}}
         );
 
         if (!(edit && edit.matchedCount)) return res.status(503).json({message: 'Database Error'});
