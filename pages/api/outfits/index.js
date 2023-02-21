6b492dfc17c3fb5e8f4f0e8bbbf2e238aa3e8c75
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-21 23:55:12 +0100

    Creating Outfit Posts : sent image and token to backend

--- pages/api/outfits/index.js
+++ pages/api/outfits/index.js
@@ -1,5 +1,12 @@
 import { posts } from '/mockData'
 
 export default function Handler(req, res) {
-    return res.status(200).json(posts);
+    if (req.method === 'GET') {
+        return res.status(200).json(posts);
+    }
+
+    if (req.method === 'POST') {
+        console.log(req.body.token);
+        return res.status(200).json(req.body);
+    }
 }
--- pages/new-outfit.js
+++ pages/new-outfit.js
@@ -2,7 +2,7 @@ import server from "@/config";
 import axios from "axios";
 import nookies from 'nookies';
 import { getSession } from "next-auth/react";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
 
 import FormData from 'form-data';
@@ -16,12 +16,19 @@ import styles from "../styles/Home.module.css";
 
 const newOutfit = () => {
   const [loading, setLoading] = useState(false)
+  const [upLoading, setUpLoading] = useState(false)
 
   const [images, setImages] = useState([{}, {}, {}, {}]);
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   
   const [savedImages, setSavedImages] = useState();
   
+  const [isUser, setIsUser] = useState();
+
+  useEffect(() => {
+    const user_token = localStorage.getItem("authentication");
+    user_token && setIsUser(user_token);
+  }, []);
   
   //Functions
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels, index) => {
@@ -113,20 +120,35 @@ const newOutfit = () => {
     })
     if (filteredImages.every((image) => image.error === false)){
       filteredImages.forEach(async (image) => {
-        setLoading(true);
+        setUpLoading(true);
         await axios.post('http://localhost:5000/api/upload', image.formData)
           .then((response) => {
-            const sentImages = filteredImages.map((image) => {
+            const toSentImages = filteredImages.map((image) => {
               return {
                 url: response.data.url,
-                deleteUrl: response.data.delete_url,
+                delete: response.data.delete_url,
                 position: image.position
               }
             })
-            setSavedImages(sentImages)
+            setSavedImages(toSentImages)
+            const sendOutfit = async() => {
+              const toSentData = {
+                image: toSentImages,
+                token: isUser.replace('"', '')
+              }
+              await axios.post('/api/outfits', JSON.stringify(toSentData), {
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              });
+            }
+            setLoading(true)
+            sendOutfit();
+            setLoading(false)
+
           })
           .catch((error) => alert(error.message))
-        setLoading(false);
+        setUpLoading(false);
         })
     } else {
       alert('Please crop your images!')
@@ -517,16 +539,17 @@ const newOutfit = () => {
 
               <div className="flex justify-center items-center my-4">
                 <button onClick={addOutfit} className="h-12 w-44 text-center bg-my-purple rounded-3xl">
-                  {!loading
+                  {!(loading || upLoading)
                   ? <p className="font-display text-white text-xl">Add</p>
                   : 
                   <div className="text-center">
-                      <div role="status">
+                      <div className="flex justify-center items-center" role="status">
                           <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-100 fill-black" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                               <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                           </svg>
-                          <span className="sr-only">Loading...</span>
+                          {upLoading && <span className="font-display text-white text-xl">Uploading ...</span>}
+                          {loading && <span className="font-display text-white text-xl">Loading ...</span>}
                       </div>
                   </div>}
                 </button>
