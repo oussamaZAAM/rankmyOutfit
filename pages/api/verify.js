59648263722dbae2c35450b1f7b5ca533aa59557
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-25 21:53:45 +0100

    Verifying token using also headers/authorization

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -3,10 +3,20 @@ import { serialize } from "cookie";
 import { verify } from "jsonwebtoken";
 
 export default async function Handler(req, res) {
-    if (req.method === 'POST') {
+    if (req.method === 'GET') {
         try {
-            const jwt = req.body.token;
-            verify(jwt, process.env.JWT_SECRET);
+            const { cookies } = req;
+            const cookieToken = cookies.authentication;
+            const authToken = req.headers.authorization;
+            // if (authToken && verify(authToken, process.env.JWT_SECRET)) {
+            //     return res.status(200).json({message: "Valid Token"});
+            // }
+            console.log(authToken)
+            if (authToken) {
+                verify(authToken, process.env.JWT_SECRET)
+            } else {
+                verify(cookieToken, process.env.JWT_SECRET)
+            }
             return res.status(200).json({message: "Valid Token"});
         } catch (e) {
             const serialised = serialize("authentication", null, {
