ddc30fc8b7051bc3efb7d5d12966f2739b73539f
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-26 18:11:56 +0100

    Merge google auth with jwt's + Clean Code 2

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -5,72 +5,91 @@ import jwt_decode from "jwt-decode";
 import { getSession } from "next-auth/react";
 
 export default async function Handler(req, res) {
-    const session = await getSession({req});
-    var verification;
+  const session = await getSession({ req });
+  var verification;
 
-    const { cookies } = req;
-    const jwt = cookies.authentication;
-    try{
-        verify(jwt, process.env.JWT_SECRET);
-        verification = true;
-    } catch(e) {
-        verification = false;
+  const { cookies } = req;
+  const jwt = cookies.authentication;
+  try {
+    verify(jwt, process.env.JWT_SECRET);
+    verification = true;
+  } catch (e) {
+    verification = false;
+  }
+
+  if (session && session.user && session.expires) {
+    if (req.method === "PUT") {
+      const user = await Users.findOne({ email: session.user.email });
+
+      if (!user) return res.status(404).json({ message: "User not found" });
+
+      const edit = await Users.updateOne({ email: session.user.email });
+
+      if (!(edit && edit.matchedCount))
+        return res.status(503).json({ message: "Database Error" });
+
+      return res
+        .status(200)
+        .json({ message: "Please change your original account's Image" });
     }
-    
-    if (session && session.user && session.expires) {
-        if (req.method === 'PUT') { 
-            const user = await Users.findOne({email: session.user.email});
-
-            if (!user) return res.status(404).json({message: 'User not found'});
-
-            const edit = await Users.updateOne(
-                {email: session.user.email}
-            )
-
-            if (!(edit && edit.matchedCount)) return res.status(503).json({message: 'Database Error'});
-
-            return res.status(200).json({message: "Please change your original account's Image"});
-        }
-    }
+  } else {
     if (verification) {
-        if (req.method === 'PUT') { 
-            const user = await Users.findOne({email: req.body.email});
+      if (req.method === "PUT") {
+        const user = await Users.findOne({ email: req.body.email });
 
-            if (!user) return res.status(404).json({message: 'User not found'});
+        if (!user) return res.status(404).json({ message: "User not found" });
 
-            const edit = await Users.updateOne(
-                {email: req.body.email},
-                { $set: {"image.url": req.body.image.url, "image.delete": req.body.image.url, "image.position": req.body.image.position}}
-            )
+        const edit = await Users.updateOne(
+          { email: req.body.email },
+          {
+            $set: {
+              "image.url": req.body.image.url,
+              "image.delete": req.body.image.url,
+              "image.position": req.body.image.position,
+            },
+          }
+        );
 
-            if (!(edit && edit.matchedCount)) return res.status(503).json({message: 'Database Error'});
+        if (!(edit && edit.matchedCount))
+          return res.status(503).json({ message: "Database Error" });
 
-            return res.status(200).json({message: 'Image updated successfully'});
+        return res.status(200).json({ message: "Image updated successfully" });
+      }
+
+      if (req.method === "GET") {
+        var decoded = jwt_decode(jwt);
+        const user = await Users.findOne({ email: decoded.email });
+
+        if (!user._id) {
+          const serialised = serialize("authentication", null, {
+            httpOnly: true,
+            secure: process.env.NEXT_ENV !== "dev",
+            sameSite: "strict",
+            maxAge: -1,
+            path: "/",
+          });
+          res.setHeader("Set-Cookie", serialised);
+          return res.status(404).json({ message: "User not found" });
         }
 
-        if (req.method === 'GET') {
-            var decoded = jwt_decode(jwt);
-            const user = await Users.findOne({email: decoded.email});
+        const requestData = {
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        };
 
-            if (!user._id) {
-                const serialised = serialize("authentication", null, {
-                    httpOnly: true,
-                    secure: process.env.NEXT_ENV !== "dev",
-                    sameSite: "strict",
-                    maxAge: -1,
-                    path: "/",
-                });
-                res.setHeader("Set-Cookie", serialised);
-                return res.status(404).json({message: 'User not found'})
-            }
-
-            const requestData = {
-                name: user.name,
-                email: user.email,
-                image: user.image
-            }
-
-            res.status(200).json(requestData);
-        }
+        res.status(200).json(requestData);
+      }
+    } else {
+      const serialised = serialize("authentication", null, {
+        httpOnly: true,
+        secure: process.env.NEXT_ENV !== "dev",
+        sameSite: "strict",
+        maxAge: -1,
+        path: "/",
+      });
+      res.setHeader("Set-Cookie", serialised);
+      return res.status(501).json({ message: "Invalid Token" });
     }
+  }
 }
