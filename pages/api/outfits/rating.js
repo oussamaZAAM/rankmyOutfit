c6991087b2fef6338d335faaf18ac6a4b54e4040
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-09 01:16:38 +0000

    Handle rating multiple images of outfits and storing in the DB

--- pages/api/outfits/rating.js
+++ pages/api/outfits/rating.js
@@ -3,7 +3,6 @@ import jwt_decode from "jwt-decode";
 import { verify } from "jsonwebtoken";
 import { getSession } from "next-auth/react";
 import Outfits from "@/model/OutfitSchema";
-import Users from "@/model/UserSchema";
 
 // Update Rating values
 export default async function Handler(req, res) {
@@ -19,41 +18,80 @@ export default async function Handler(req, res) {
     } catch (e) {
       verification = false;
     }
-
-    //Check if the user is authenticated through (Google / Facebook / ...) Provider
-    if (session && session.user && session.expires) {
-      //Rate Posts by (Google / Facebook / ...) users
-      const thisOutfit = await Outfits.findById(req.body._id);
-      const allRaters = thisOutfit.raters.filter(
-        (rater) => rater.email !== session?.user?.email
-      );
-      allRaters.push({
-        email: session?.user?.email,
-        rating: req.body.myRating.rating,
-      });
-      const edit = await Outfits.updateOne(
-        { _id: req.body._id },
-        {
-          $set: {
-            raters: allRaters,
-          },
-        }
-      );
-      if (!(edit && edit.matchedCount))
-        return res.status(503).json({ message: "Database Error" });
-
-      return res.status(200).json({ message: "Rating updated successfully" });
-    } else {
-      if (verification) {
-        // Rate Posts by JWT users
-        var decodedUser = jwt_decode(jwt);
+    
+    if (req.body.myBest && !req.body.myRating){
+      //Check if the user is authenticated through (Google / Facebook / ...) Provider
+      if (session && session.user && session.expires) {
+        //Rate Posts by (Google / Facebook / ...) users
         const thisOutfit = await Outfits.findById(req.body._id);
         const allRaters = thisOutfit.raters.filter(
-          (rater) => !(rater._id.equals(decodedUser._id))
-          
+          (rater) => rater.email !== session?.user?.email
         );
         allRaters.push({
-          _id: decodedUser._id,
+          email: session?.user?.email,
+          best: req.body.myBest.best,
+        });
+        const edit = await Outfits.updateOne(
+          { _id: req.body._id },
+          {
+            $set: {
+              raters: allRaters,
+            },
+          }
+        );
+        if (!(edit && edit.matchedCount))
+          return res.status(503).json({ message: "Database Error" });
+
+        return res.status(200).json({ message: "Rating updated successfully" });
+      } else {
+        if (verification) {
+          // Rate Posts by JWT users
+          var decodedUser = jwt_decode(jwt);
+          const thisOutfit = await Outfits.findById(req.body._id);
+          const allRaters = thisOutfit.raters.filter(
+            (rater) => !(rater._id.equals(decodedUser._id))
+            
+          );
+          allRaters.push({
+            _id: decodedUser._id,
+            best: req.body.myBest.best,
+          });
+          console.log(allRaters)
+          const edit = await Outfits.updateOne(
+            { _id: req.body._id },
+            {
+              $set: {
+                raters: allRaters,
+              },
+            }
+          );
+          if (!(edit && edit.matchedCount))
+            return res.status(503).json({ message: "Database Error" });
+
+          return res.status(200).json({ message: "Rating updated successfully" });
+        } else {
+          //Delete non authenticated user's cookies
+          const serialised = serialize("authentication", null, {
+            httpOnly: true,
+            secure: process.env.VERCEL_ENV !== "development",
+            sameSite: "strict",
+            maxAge: -1,
+            path: "/",
+          });
+          res.setHeader("Set-Cookie", serialised);
+          return res.status(501).json({ message: "Invalid Token" });
+        }
+      }
+    } else {
+      //Check if the user is authenticated through (Google / Facebook / ...) Provider
+      if (session && session.user && session.expires) {
+        //Rate Posts by (Google / Facebook / ...) users
+        const thisOutfit = await Outfits.findById(req.body._id);
+        const allRaters = thisOutfit.raters.filter(
+          (rater) => rater.email !== session?.user?.email
+        );
+        allRaters.push({
+          email: session?.user?.email,
           rating: req.body.myRating.rating,
         });
         const edit = await Outfits.updateOne(
@@ -69,16 +107,42 @@ export default async function Handler(req, res) {
 
         return res.status(200).json({ message: "Rating updated successfully" });
       } else {
-        //Delete non authenticated user's cookies
-        const serialised = serialize("authentication", null, {
-          httpOnly: true,
-          secure: process.env.VERCEL_ENV !== "development",
-          sameSite: "strict",
-          maxAge: -1,
-          path: "/",
-        });
-        res.setHeader("Set-Cookie", serialised);
-        return res.status(501).json({ message: "Invalid Token" });
+        if (verification) {
+          // Rate Posts by JWT users
+          var decodedUser = jwt_decode(jwt);
+          const thisOutfit = await Outfits.findById(req.body._id);
+          const allRaters = thisOutfit.raters.filter(
+            (rater) => !(rater._id.equals(decodedUser._id))
+            
+          );
+          allRaters.push({
+            _id: decodedUser._id,
+            rating: req.body.myRating.rating,
+          });
+          const edit = await Outfits.updateOne(
+            { _id: req.body._id },
+            {
+              $set: {
+                raters: allRaters,
+              },
+            }
+          );
+          if (!(edit && edit.matchedCount))
+            return res.status(503).json({ message: "Database Error" });
+
+          return res.status(200).json({ message: "Rating updated successfully" });
+        } else {
+          //Delete non authenticated user's cookies
+          const serialised = serialize("authentication", null, {
+            httpOnly: true,
+            secure: process.env.VERCEL_ENV !== "development",
+            sameSite: "strict",
+            maxAge: -1,
+            path: "/",
+          });
+          res.setHeader("Set-Cookie", serialised);
+          return res.status(501).json({ message: "Invalid Token" });
+        }
       }
     }
   }
